eb784517b099d07853c005948fc903e2dbcaef92
tikimips <tikimips@gmail.com>
2025-07-19 10:10:03 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
