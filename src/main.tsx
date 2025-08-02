bb2c3ef68329171a300c7f73fa148e76db4b56ed
tikimips <tikimips@gmail.com>
2025-08-02 12:57:48 -0700

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Hello, PromptShop!</h1>
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
