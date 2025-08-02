b9fdca2116d75df4742b63993e14785b826eda6f
tikimips <tikimips@gmail.com>
2025-08-02 12:38:03 -0700

    move index.html back to public and fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './', // root is now the project root
   plugins: [react()],
+  root: '.', // leave this as is
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html',
+    },
   },
 })
