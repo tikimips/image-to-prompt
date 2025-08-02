0476cf045c728a961b5d184a562e5374ba10036a
tikimips <tikimips@gmail.com>
2025-08-02 11:26:27 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
