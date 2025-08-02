55a56fef2dfc1852b94085c34065c317bc21ab5e
tikimips <tikimips@gmail.com>
2025-08-02 13:24:49 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // or wherever your global styles are
 
-const root = document.getElementById('root');
-
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
