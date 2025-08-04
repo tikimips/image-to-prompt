ef2b5878bdce5cbb7670ac1ffa63140533ad65eb
tikimips <tikimips@gmail.com>
2025-08-03 22:14:53 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import SimpleUpload from './components/SimpleUpload'; // Make sure this path is correct
+import SimpleUpload from './components/SimpleUpload';
 
 function App() {
-  return <SimpleUpload />;
+  return (
+    <main className="min-h-screen bg-white text-black flex items-center justify-center">
+      <SimpleUpload />
+    </main>
+  );
 }
 
 export default App;
